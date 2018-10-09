85570558d27e5d7fe49a49dc30a8163b98aa3341
woeishi <woei@refect.net>
2018-10-09 22:59:43 +0200

    need a refcounter
    frame will be lending its SRV instead of copying. ref counting in case of multiple visible frames

---
+++ RefCounter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VVVV.DX11.ImagePlayer
+{
+    public class RefCounter
+    {
+        public class Handle : IDisposable
+        {
+            RefCounter rc;
+            internal Handle(RefCounter refCounter)
+            {
+                rc = refCounter;
+                rc.count++;
+            }
+            private Handle()
+            { }
+            public void Dispose() => rc.count--;
+        }
+
+        int count = 0;
+        public IDisposable Use() => new Handle(this);
+
+        public bool Free => count <= 0;
+    }
+}
